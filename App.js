8e2483a96ce0b576e47a1accdb2ed568aef82122
Rudy <RodriguezLucha@gmail.com>
2019-07-02 18:42:55 -0700

    Add key generation

--- App.js
+++ App.js
@@ -1,33 +1,45 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import Biometrics from 'react-native-biometrics';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component{
+  componentDidMount() {
+    Biometrics.isSensorAvailable()
+      .then((biometryType) => {
+        if (biometryType === Biometrics.TouchID) {
+          console.log('TouchID is supported');
+        } else if (biometryType === Biometrics.FaceID) {
+          console.log('FaceID is supported');
+        } else {
+          console.log('Biometrics not supported');
+        }
+      });
+  }
+
+  createKey(){
+    console.log('creating key');
+    Biometrics.createKeys('Confirm fingerprint')
+      .then((publicKey) => {
+        console.log(publicKey);
+      });
+  }
+
   render() {
+    console.log('the state:', this.state);
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        {/* <Button
+          onClick={this.createKey}
+          style={styles.welcome}>Create Finger Print</Button> */}
+        <Button
+          onPress={this.createKey}
+          title="Press Me"/>
       </View>
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
